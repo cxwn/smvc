22495428b32b6725506fd55c026e5524fce7ee49
ivandu <mrivandu@hotmail.com>
2021-10-26 02:08:37 +0800

    更新

---
+++ smvc-controller/src/main/java/com/ivandu/smvc/controller/ControllerTest.java
@@ -0,0 +1,18 @@
+package com.ivandu.smvc.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ControllerTest implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("msg", "ControllerTest!");
+        modelAndView.setViewName("test");
+        return modelAndView;
+    }
+}
