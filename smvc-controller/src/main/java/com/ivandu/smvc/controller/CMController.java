14b1d5244ec5dca46097b832ec9837ecc10b8c4b
cxwn <mrivandu@hotmail.com>
2021-10-26 21:32:19 +0800

    fix: controller

---
+++ smvc-controller/src/main/java/com/ivandu/smvc/controller/CMController.java
@@ -0,0 +1,19 @@
+package com.ivandu.smvc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CMController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("msg", "This is ChinaMobile SpringMVC.");
+        modelAndView.setViewName("test");
+        return modelAndView;
+    }
+}
